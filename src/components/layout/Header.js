a9a376472974f3fc966bacf00d1acb55a25b48ac
pyavchik <pyavchik@gmail.com>
2018-10-16 23:34:13 +0300

    last version

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,16 +1,31 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-const Header = (props) => {
+const Header = props => {
   const { branding } = props;
   return (
-    <nav className="navbar navbar-expend-sm navbar-dark bg-danger mb-3 py-0">
+    <nav className="navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0">
       <div className="container">
-        <a href="/" className="navbar-brand">{branding}</a>
+        <a href="/" className="navbar-brand">
+          {branding}
+        </a>
         <div>
           <ul className="navbar-nav mr-auto">
             <li className="nav-item">
-              <a href="/" className="nav-link">Home</a>
+              <Link to="/" className="nav-link">
+                <i className="fas fa-home" /> Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/contact/add" className="nav-link">
+                <i className="fas fa-plus" /> Add
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/about" className="nav-link">
+                <i className="fas fa-question" /> About
+              </Link>
             </li>
           </ul>
         </div>
@@ -21,9 +36,9 @@ const Header = (props) => {
 
 Header.defaultProps = {
   branding: 'My App'
-}
+};
 
-Header.prototypes = {
+Header.propTypes = {
   branding: PropTypes.string.isRequired
 };
 
--- src/context.js
+++ src/context.js
@@ -1,4 +1,5 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
 const Context = React.createContext();
 
@@ -7,49 +8,49 @@ const reducer = (state, action) => {
     case 'DELETE_CONTACT':
       return {
         ...state,
-        contacts: state.contacts.filter(contact => 
-        contact.id !== action.payload)
-      }
+        contacts: state.contacts.filter(
+          contact => contact.id !== action.payload
+        )
+      };
+    case 'ADD_CONTACT':
+      return {
+        ...state,
+        contacts: [action.payload, ...state.contacts]
+      };
+    case 'UPDATE_CONTACT':
+      return {
+        ...state,
+        contacts: state.contacts.map(
+          contact =>
+            contact.id === action.payload.id
+              ? (contact = action.payload)
+              : contact
+        )
+      };
     default:
-    return state;
-  }    
-}
+      return state;
+  }
+};
 
 export class Provider extends Component {
   state = {
-    contacts: [
-      {
-        id: 1,
-        name: 'John Doe',
-        email: 'jdoe@gmail.com',
-        phone: '555-555-5555'
-      },
-      {
-        id: 2,
-        name: 'Karen Williams',
-        email: 'karen@gmail.com',
-        phone: '222-555-2222'
-      },
-      {
-        id: 3,
-        name: 'Henry Johnson',
-        email: 'henry@gmail.com',
-        phone: '75-5545-5475'
-      }
-    ],
-    dispatch: action => {
-      this.setState(state => reducer(state, action))
-    }
+    contacts: [],
+    dispatch: action => this.setState(state => reducer(state, action))
   };
+
+  async componentDidMount() {
+    const res = await axios.get('https://jsonplaceholder.typicode.com/users');
+
+    this.setState({ contacts: res.data });
+  }
 
   render() {
     return (
       <Context.Provider value={this.state}>
         {this.props.children}
       </Context.Provider>
-    )
+    );
   }
 }
 
-export const Consumer = Context.Consumer; 
- 
+export const Consumer = Context.Consumer;
